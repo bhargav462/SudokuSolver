b7ba82b7c19580cef84271e752cf8532aa07420a
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-08-27 01:06:10 +0530

    Sudoku Solver without proper testing

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname,'/public');
+app.use(express.static(publicDirectoryPath))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res) => {
+    res.render('index');
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,(res) => {
+    console.log(`Server is up on port ${port}`);
+})
+
